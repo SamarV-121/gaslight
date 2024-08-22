cd03e7494494e7a78574cb7e0aa0210da0d33f85
SamarV-121 <samar@samarv121.dev>
2024-08-22 16:51:44 +0530

    Reformat
    Change-Id: If0a487c47cb14eb9bd171361840a031f9bde611d

--- gaslight/asgi.py
+++ gaslight/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gaslight.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gaslight.settings")
 
 application = get_asgi_application()
--- gaslight/wsgi.py
+++ gaslight/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gaslight.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gaslight.settings")
 
 application = get_wsgi_application()
